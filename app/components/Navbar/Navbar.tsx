1da18e2893c6b0931b852fb6d4552eb78a29e693
Dipongkor-Roy <juniordipu@gmail.com>
2024-06-06 02:46:13 +0600

    get session in client

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,47 +1,79 @@
-import Link from 'next/link';
-import { title } from 'process';
-import React from 'react';
+"use client";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
-    const links=[{
-        slug:'/users',
-        title:"Users"
+  const { status, data: session } = useSession();
+  const links = [
+    {
+      slug: "/users",
+      title: "Users",
     },
-// {
-//     // slug:'/api/auth/signin',
-//     // title:'LogIn'
-// }
-    ]
-    return (
-        <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                {
-                    links.map(link=>(<li key={link.slug}><Link href={link.slug}>{link.title}</Link></li>))
-                }
-             
-             
-            </ul>
+    // {
+    //     // slug:'/api/auth/signin',
+    //     // title:'LogIn'
+    // }
+  ];
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <Link href='/' className="btn btn-ghost text-xl">Next App</Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-          {
-                    links.map(link=>(<li key={link.slug}><Link href={link.slug}>{link.title}</Link></li>))
-                }
-           
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links.map((link) => (
+              <li key={link.slug}>
+                <Link href={link.slug}>{link.title}</Link>
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="navbar-end">
-        <button className='btn btn-sm'><Link href='/api/auth/signin'>LogIn</Link></button>
-        </div>
+        <Link href="/" className="btn btn-ghost text-xl">
+          Next App
+        </Link>
       </div>
-    );
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links.map((link) => (
+            <li key={link.slug}>
+              <Link href={link.slug}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {status === "authenticated" ? (
+          session && session.user ? (
+            <button className="btn btn-sm">{session.user.name}</button>
+          ) : (
+            <button className="btn btn-sm">
+              <Link href="/api/auth/signin">LogIn</Link>
+            </button>
+          )
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
-interface Props{
-    params:{id:number}
+import React from "react";
+interface Props {
+  params: { id: number };
 }
-const userPage = ({params:{id}}:Props) => {
+const userPage = ({ params: { id } }: Props) => {
   return (
-    <div className='m-3'>
-      <h2 className='text-center'>Welcome To Next App</h2>
-<div className='text-center mt-3'>
-
-</div>
+    <div className="m-3">
+      <h2 className="text-center">Welcome To Next App</h2>
+      <div className="text-center mt-3"></div>
     </div>
-  )
-}
+  );
+};
 
-export default userPage
+export default userPage;
